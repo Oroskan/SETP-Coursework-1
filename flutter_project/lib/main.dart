559e3e84375b8d3bca62938bf2711c80e8708b64
Gabriel Parvery <118484323+g-parvery@users.noreply.github.com>
2025-02-17 21:19:02 +0000

    build settings page

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'settings_menu.dart';
 
 void main() {
   runApp(const MainApp());
@@ -6,15 +7,16 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Account Settings',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
+      home: AccountSettings(),  // Replace the default home with your AccountSettings page
     );
   }
 }
